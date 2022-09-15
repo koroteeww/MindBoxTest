ff5a725f58c9d39590d83f4481ecf701d82e4bf8
AstemirMamhegov <64645245+AstemirMamhegov@users.noreply.github.com>
2022-09-15 13:18:08 +0300

    #3 Add First Versiom
    Выполнено первое условие задачи включающее в себя возможность высчитывание площади треугольника по трем сторонам, а так же площади круга.

--- MindBoxTestLibrary/MindBoxTestConsole/Program.cs
+++ MindBoxTestLibrary/MindBoxTestConsole/Program.cs
@@ -2,36 +2,63 @@
 
 class Program
 {
+    /* Выполните практическое задание ниже.
+     * Отклики без выполненного задания не будут рассмотрены.
+     * Пожалуйста, не пишите код внутри форм ответов, разместите его на Github и приложите ссылку.
+     * Если в задании что-то непонятно, опишите возникшие вопросы и сделанные предположения. Например, в комментариях в коде.
+     * Задание:
+     * Напишите на C# библиотеку для поставки внешним клиентам, которая умеет вычислять площадь круга по радиусу и треугольника по трем сторонам.
+     * Дополнительно к работоспособности оценим:
+     * Юнит-тесты
+     * Легкость добавления других фигур
+     * Вычисление площади фигуры без знания типа фигуры в compile-time
+     * Проверку на то, является ли треугольник прямоугольным
+     */
     static void Main(string[] args)
     {
-        int row = 0;
+        Console.WriteLine("Выберите фигуру площадь которой вы хотите высчитать: 1. Треугольник по формуле трех сторон. 2. Круг по радиусу. ");
+        int meaning = Convert.ToInt32(Console.ReadLine());
 
-        do
+        if (meaning == 1)
         {
-            if (row == 0 || row >= 25)
-                ResetConsole();
-
-            string? input = Console.ReadLine();
-            if (string.IsNullOrEmpty(input)) break;
-            Console.WriteLine($"Input: {input}");
-            Console.WriteLine("Begins with uppercase? " +
-                 $"{(input.StartsWithUpper() ? "Yes" : "No")}");
-            Console.WriteLine();
-            row += 4;
-        } while (true);
-        return;
-
-        // Declare a ResetConsole local method
-        void ResetConsole()
-        {
-            if (row > 0)
-            {
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadKey();
-            }
-            Console.Clear();
-            Console.WriteLine($"{Environment.NewLine}Press <Enter> only to exit; otherwise, enter a string and press <Enter>:{Environment.NewLine}");
-            row = 3;
+            string triangleSquareResult = TriangleSquareResult();
+            Console.WriteLine("Площадь треугольника равна: " + triangleSquareResult);
         }
+        else if (meaning == 2)
+        {
+            string circleSquareResult = CircleSquareResult();
+            Console.WriteLine("Площадь круга равна: " + circleSquareResult);
+        }
+
+        Console.ReadKey();
+    }
+
+    public static string TriangleSquareResult()
+    {
+        TestLibrary library = new TestLibrary();
+        Console.WriteLine("Введите длины сторон треугольника: ");
+
+        Console.Write("Сторона А: ");
+        double triangleA = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Сторона B: ");
+        double triangleB = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Сторона C: ");
+        double triangleC = Convert.ToDouble(Console.ReadLine());
+
+        string result = library.TriangleSquare(triangleA, triangleB, triangleC).ToString();
+        return result;
+    }
+
+    public static string CircleSquareResult()
+    {
+        TestLibrary library = new TestLibrary();
+
+        Console.Write("Введи длину радиуса круга: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+
+        string result = library.CircleSquare(radius).ToString();
+        return result;
     }
 }
---
+++ MindBoxTestLibrary/MindBoxTestLibraryClass/TestLibrary.cs
@@ -0,0 +1,23 @@
+﻿namespace MindBoxTestLibraryClass;
+
+public class TestLibrary
+{
+    public double TriangleSquare(double triangleA, double triangleB, double triangleC)
+    {
+        // Вычисление полупериметра.
+        double halfPerimeter = (triangleA + triangleB + triangleC) / 2;
+
+        //Вычисление площади
+        double squareResult = Math.Sqrt(halfPerimeter * (halfPerimeter - triangleA) * (halfPerimeter - triangleB) * (halfPerimeter - triangleC));
+
+        return squareResult;
+    }
+
+    public double CircleSquare(double radius)
+    {
+
+        double squareResult = Math.PI * (radius * radius);
+
+        return squareResult;
+    }
+}
